b924d9aaf87e44aa502a7a783ee44b52bce1d106
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-08-09 19:33:21 -0400

    added commentation

--- hello.go
+++ hello.go
@@ -1,9 +1,13 @@
 package main
 
+// choose a package from library to use inside Go application
+// fmt is one of the most related in go
+// no commas (,) after each package
 import (
 	"fmt"
 )
 
+// to declare function, simply write the func before the key word. The first function has to be main
 func main() {
 	fmt.Println("Hello Go!")
 }
